63f5c4bacb96154833f78c0a26e75047e3a2f8e8
Rubonic <dannykraemer099@gmail.com>
2022-05-13 10:13:03 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function addLike1() {
+    var value = document.getElementById("likeCount1").innerHTML;
+    value++;
+    document.getElementById("likeCount1").innerHTML = value;
+}
+
+function addLike2() {
+    var value = document.getElementById("likeCount2").innerText;
+    value++;
+    document.getElementById("likeCount2").innerHTML = value;
+}
+
+function addLike3() {
+    var value = document.getElementById("likeCount3").innerText;
+    value++;
+    document.getElementById("likeCount3").innerHTML = value;
+}
